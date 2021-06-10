c3f6c31164df4a5e120112edff946251fb50e481
dulfrey <wdhernandezh@unal.edu.co>
2021-06-10 18:05:15 -0500

    viewer

--- src/app/viewer/viewer.component.ts
+++ src/app/viewer/viewer.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { create } from 'ipfs-http-client';
 @Component({
   selector: 'app-viewer',
   templateUrl: './viewer.component.html',
   styleUrls: ['./viewer.component.scss']
 })
 export class ViewerComponent implements OnInit {
-
+  title =  'El titulo'
+  urlvideo = 'url video'
+  description = 'descripcióin'
+  urlButton = 'url boton'
+  textButton = 'texto boton'
   constructor() { }
 
-  ngOnInit() {
+
+  async init(){
+    //const IPFS = ('ipfs-mini');
+    const ipfs = create({host:'http://ipfs.infura.io:5001'});
+    const data = "this is the first free funnel test";
+    const file = await ipfs.add(data);
+    console.log(file)
+  }
+   ngOnInit() {
+    this.init()
+    
   }
 
 }
